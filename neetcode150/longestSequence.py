c7d4ce192e4003b498aba035207f59d084216314
anothergrind <ozorjikj@gmail.com>
2026-03-17 01:12:00 -0500

    day 9, my solution but it don't work idk why

--- neetcode150/longestSequence.py
+++ neetcode150/longestSequence.py
@@ -12,3 +12,18 @@ def longestConsecutive(self, nums: list[int]) -> int:
                 length += 1
             longest = max(length, longest)
     return longest
+
+def mySolution(self, nums: list[int]) -> int:
+    if len(nums) == 0:
+        return 0
+    
+    counter = 0
+    remove_dups = set(nums)
+    backList = list(remove_dups)
+    
+    for i in range(0, len(backList)):
+        for j in range(0, len(backList)):
+            if backList[i] + 1 == backList[j]:
+                counter = counter + 1
+
+    return counter
