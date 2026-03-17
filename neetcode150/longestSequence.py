7a50df6b68ece0a9c0e17fd68fa7dabc5a13ef2a
anothergrind <ozorjikj@gmail.com>
2026-03-17 01:01:50 -0500

    day 9 longest consecutive sequence

---
+++ neetcode150/longestSequence.py
@@ -0,0 +1,14 @@
+# hash set solution
+# solves in O(n) time
+
+def longestConsecutive(self, nums: list[int]) -> int:
+    numSet = set(nums)
+    longest = 0
+
+    for num in numSet:
+        if (num - 1) not in numSet:
+            length = 1
+            while (num + length) in numSet:
+                length += 1
+            longest = max(length, longest)
+    return longest
