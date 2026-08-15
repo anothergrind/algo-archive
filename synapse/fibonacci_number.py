c80346cc09d3d81dadb1d5a216954d2908218901
anothergrind <ozorjikj@gmail.com>
2026-08-14 19:32:19 -0500

    Fibonacci Number

---
+++ synapse/fibonacci_number.py
@@ -0,0 +1,11 @@
+from typing import Any, Dict, List, Optional
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        return self.fib(n - 2) + self.fib(n - 1)
